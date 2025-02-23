23e8a0fd4a4071e42074df1a7ec576ca20548b15
Wahyu Patriaji <wahyupatriaji@gmail.com>
2025-02-23 19:07:36 +0700

    chore: initialize Expo project with basic configuration and setup for geo map app

---
+++ app.config.ts
@@ -0,0 +1,72 @@
+import { type ExpoConfig } from 'expo/config';
+
+export default (): ExpoConfig => {
+    const productName = 'Geo Track';
+    const pkgId = 'com.whyaji.geotrack';
+    return {
+        name: productName,
+        slug: 'whyajigeotrack',
+        version: '1.0.0',
+        orientation: 'portrait',
+        icon: './assets/images/icon.png',
+        scheme: 'myapp',
+        userInterfaceStyle: 'automatic',
+        newArchEnabled: true,
+        ios: {
+            bundleIdentifier: pkgId,
+            supportsTablet: true,
+            config: {
+                googleMapsApiKey: process.env['EXPO_PUBLIC_GOOGLE_MAPS_API_KEY'],
+            },
+        },
+        android: {
+            package: pkgId,
+            adaptiveIcon: {
+                foregroundImage: './assets/images/adaptive-icon.png',
+                backgroundColor: '#ffffff',
+            },
+            config: {
+                googleMaps: {
+                    apiKey: process.env['EXPO_PUBLIC_GOOGLE_MAPS_API_KEY'],
+                },
+            },
+        },
+        web: {
+            bundler: 'metro',
+            output: 'static',
+            favicon: './assets/images/favicon.png',
+        },
+        plugins: [
+            'expo-router',
+            [
+                'expo-splash-screen',
+                {
+                    image: './assets/images/splash-icon.png',
+                    imageWidth: 200,
+                    resizeMode: 'contain',
+                    backgroundColor: '#ffffff',
+                },
+            ],
+            [
+                'expo-location',
+                {
+                    locationAlwaysAndWhenInUsePermission: `Allow ${productName} to use your location.`,
+                },
+            ],
+        ],
+        experiments: {
+            typedRoutes: true,
+        },
+        extra: {
+            eas: {
+                projectId: process.env['EXPO_PUBLIC_PROJECT_ID'],
+            },
+        },
+        updates: {
+            url: `https://u.expo.dev/${process.env['EXPO_PUBLIC_PROJECT_ID']}`,
+        },
+        runtimeVersion: {
+            policy: 'appVersion',
+        },
+    };
+};
